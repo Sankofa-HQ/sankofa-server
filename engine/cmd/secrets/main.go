16b317a77e88bf97061a883cf4fe474b8202535d
saytoonz <35128520+saytoonz@users.noreply.github.com>
2026-02-19 15:20:31 +0000

    Secret gen

---
+++ engine/cmd/secrets/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+)
+
+func main() {
+	// Generate 32 bytes (64 hex characters) of random data
+	bytes := make([]byte, 32)
+	if _, err := rand.Read(bytes); err != nil {
+		fmt.Printf("Error generating secure random bytes: %v\n", err)
+		return
+	}
+
+	secret := hex.EncodeToString(bytes)
+	fmt.Println("Your new secure API_SECRET:")
+	fmt.Println(secret)
+}
